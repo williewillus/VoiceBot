f0089658c47e354b50e35a880d461d740f38e184
Calvin Ly <cwly12@gmail.com>
2016-04-21 05:12:02 -0500

    changed to use actionlib

--- scripts/move_base_voice.py
+++ scripts/move_base_voice.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python
-from __future__ import division
 import rospy
 import math
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
+import roslib
+roslib.load_manifest('move_base')
+import actionlib
+import tf
+
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+
+goal = MoveBaseGoal()
 
 #based on voice_cmd_vel
 class move_base_voice:
     def __init__(self):
 	rospy.on_shutdown(self.cleanup)
-	self.msg = Twist()
-	
-	self.pub_ = rospy.Publisher('cmd_vel', Twist)
+
 	rospy.Subscriber('recognizer/output', String, self.speechCb)
-	
-	r = rospy.Rate(10.0)
-	
-	while not rospy.is_shutdown():
-	    self.pub_.publish(self.msg)
-	    r.sleep()
 
     def speechCb(self, msg):
 	#rospy.loginfo(msg.data)
 	#commands go here
 	if msg.data.find("turn") > -1:
 	    if msg.data.find("ninety degrees") > -1:
-	    	self.msg.angular.z = self.msg.angular.z + math.radians(90)
-
-	self.pub_.publish(self.msg)
+		q = tf.transformations.quaternion_from_euler(0, 0, math.radians(90))
+		
+	    	goal.target_pose.pose.orientation.x = q[0]
+		goal.target_pose.pose.orientation.y = q[1]
+		goal.target_pose.pose.orientation.z = q[2]
+		goal.target_pose.pose.orientation.w = q[3]
+	
     def cleanup(self):
-	twist = Twist()
-	self.pub_.publish(twist)
+	goal = MoveBaseGoal()
 
 if __name__ == '__main__':
     rospy.init_node('move_base_voice')
     try:
         move_base_voice()
+	client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+	#client.wait_for_server()
+	while not rospy.is_shutdown():
+	    client.send_goal(goal)
+	    client.wait_for_result(rospy.Duration.from_sec(5.0))
     except rospy.ROSInterruptException:
         pass
